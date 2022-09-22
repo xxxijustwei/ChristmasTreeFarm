df21518c3dce036d2c9f029c8f30c20b1a2c71db
justwei <admin@taylorswiftcn.com>
2022-09-22 17:13:34 +0800

    🚧 update

--- server/contracts/util/Utils.sol
+++ server/contracts/util/Utils.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 library Utils {
 
+    // unsafe
     function random(uint number) public view returns(uint) {
         return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))) % number;
     }
