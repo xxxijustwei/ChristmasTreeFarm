2c45afffc29de634d1fdc779a7faaaa767c60912
justwei <admin@taylorswiftcn.com>
2022-10-14 19:41:57 +0800

    🚧 update contract

--- server/contracts/ChristmasStocking.sol
+++ server/contracts/ChristmasStocking.sol
@@ -5,121 +5,181 @@ import "./randomness/Randomness.sol";
 import "./randomness/RandomnessConsumer.sol";
 import "./utils/Uint8a32.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 uint64 constant FULFILLMENT_GAS_LIMIT = 100000;
 uint constant MIN_FEE = FULFILLMENT_GAS_LIMIT * 2 gwei;
 uint constant DEPOSIT = 1 ether;
+uint8 constant MAX_AMOUNT = 32;
+uint8 constant MAX_KEY_LENGTH = 8;
 
-contract ChristmasStocking is RandomnessConsumer {
+contract ChristmasStockingV2 is RandomnessConsumer {
 
     using Uint8a32 for uint;
     using SafeMath for uint;
 
-    address public factory;
-    address public owner;
-    bytes32 private salt;
-
+    uint private requestCounter;
+    bytes32 private constant SALT = "taylor swift";
     Randomness private randomness = Randomness(0x0000000000000000000000000000000000000809);
 
-    uint private key;
-    uint public originAmount;
-    uint public originMoney;
-    uint public currentAmount;
-    uint public currentMoney;
+    struct Stocking {
+        uint amount;
+        uint money;
+        uint remainAmount;
+        uint remainMoney;
 
-    uint public requestID = 2 ** 64 - 1;
-    uint private result;
-    bool public done;
+        uint randomWord;
 
-    uint public participateCount;
-    mapping(address => bool) public participateIn;
+        mapping(address => bool) participateIn;
+    }
 
-    modifier canParticipate() {
-        if (
-            currentAmount == 0 ||
-            currentMoney == 0 ||
-            participateIn[msg.sender] ||
-            !done
-        ) revert CantParticipate();
+    mapping(bytes32 => bool) private contains;
+    mapping(bytes32 => bool) private finished;
+    mapping(bytes32 => address) private owners;
+    mapping(bytes32 => Stocking) private presents;
+
+    mapping(uint => bytes32) requestToIdent;
+    mapping(bytes32 => uint) identToRequest;
+
+    event PresentsCreateEvent(bytes32 indexed ident, uint indexed key, uint indexed num);
+    event PresentsParticipateEvent(bytes32 indexed ident, uint reward);
+
+    error AmountBeyondError(uint amount, uint limited);
+    error KeyLengthBeyondError(uint key, uint inputLen, uint requireLen);
+    error MoneyNotEnoughError(uint inputMoney, uint actualMoney);
+    error InvalidPresentsError();
+    error PresentsWaitingError();
+
+    modifier onlyOwner(bytes32 _ident) {
+        require(owners[_ident] == msg.sender, "Not permission");
         _;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Permission denied");
+    modifier isContain(bytes32 _ident) {
+        if (!contains[_ident]) revert InvalidPresentsError();
         _;
     }
 
-    event ParticipateEvent(address indexed participant, uint reward, uint randomWord);
-
-    error NotEnoughFee(uint value, uint required);
-    error DepositTooLow(uint value, uint required);
-    error CantParticipate();
-
-    constructor() payable RandomnessConsumer() {
-        factory = msg.sender;
+    modifier isValid(bytes32 _ident) {
+        if (!finished[_ident]) revert PresentsWaitingError();
+        _;
     }
 
-    function initialize(address _owner, uint _key, bytes32 _salt, uint _amount, uint _money) external {
-        owner = _owner;
-        salt = _salt;
-        key = _key;
-        (originAmount, currentAmount) = (_amount, _amount);
-        (originMoney, currentMoney) = (_money, _money);
+    constructor() RandomnessConsumer() {
+        requestCounter = 0;
+    }
 
-        requestID = randomness.requestLocalVRFRandomWords(
-            owner,
+    function create(uint _key, uint _amount, uint _money) external payable {
+        if (_amount > MAX_AMOUNT) revert AmountBeyondError(_amount, MAX_AMOUNT);
+
+        uint len = _getKeyLength(_key);
+        if (len > MAX_KEY_LENGTH) revert KeyLengthBeyondError(_key, len, MAX_KEY_LENGTH);
+
+        address sender = msg.sender;
+        uint value = msg.value;
+        uint money = value.sub(DEPOSIT).sub(MIN_FEE);
+        if (money != _money) revert MoneyNotEnoughError(_money, money);
+
+        requestCounter++;
+        bytes32 ident = _getIdentifier(_key);
+
+        contains[ident] = true;
+        owners[ident] = sender;
+
+        Stocking storage gift = presents[ident];
+        (gift.amount, gift.remainAmount) = (_amount, _amount);
+        (gift.money, gift.remainMoney) = (_money, _money);
+
+        uint requestID = randomness.requestLocalVRFRandomWords(
+            address(this),
             MIN_FEE,
             FULFILLMENT_GAS_LIMIT,
-            salt,
-            uint8(originAmount - 1),
+            SALT ^ bytes32(requestCounter),
+            uint8(_amount - 1),
             2
         );
+        requestToIdent[requestID] = ident;
+        identToRequest[ident] = requestID;
+
+        emit PresentsCreateEvent(ident, _key, requestCounter);
     }
 
-    function fulfillRequest() external onlyOwner {
-        require(getRequestStatus() == 2, "fulfillRequest failure");
+    function participate(bytes32 _ident) external isContain(_ident) isValid(_ident) {
+        address sender = msg.sender;
+        Stocking storage gift = presents[_ident];
+        require(gift.remainAmount != 0, "Gift is empty");
+        require(!gift.participateIn[sender], "You have already participate");
+
+        uint random = gift.randomWord.get(gift.amount - gift.remainAmount);
+
+        gift.participateIn[sender] = true;
+        gift.remainAmount--;
+
+        uint reward = gift.remainAmount == 0 ?
+            gift.remainMoney :
+            (gift.remainMoney.mul(10).div(gift.remainAmount.add(1).mul(5))).mul(random).div(100);
+        gift.remainMoney -= reward;
+
+        payable(sender).transfer(reward);
+
+        emit PresentsParticipateEvent(_ident, reward);
+    }
+
+    function drawback(bytes32 _ident) external onlyOwner(_ident) isContain(_ident) {
+        Stocking storage gift = presents[_ident];
+        require(gift.remainAmount == 0, "There are still gifts left");
+        require(address(this).balance >= 1, "Drawback failure");
+
+        uint requestID = identToRequest[_ident];
+
+        delete contains[_ident];
+        delete finished[_ident];
+        delete owners[_ident];
+        delete presents[_ident];
+        delete requestToIdent[requestID];
+        delete identToRequest[_ident];
+
+        payable(msg.sender).transfer(1 ether);
+    }
+
+    function fulfillRequest(bytes32 _ident) external onlyOwner(_ident) isContain(_ident) {
+        uint requestID = identToRequest[_ident];
+        require(uint(randomness.getRequestStatus(requestID)) == 2, "waiting for parachain");
+
         randomness.fulfillRequest(requestID);
     }
 
-    function increaseRequestFee() external payable onlyOwner {
-        randomness.increaseRequestFee(requestID, msg.value);
-    }
-
-    function participate() external canParticipate() {
-        address sender = msg.sender;
-        participateIn[sender] = true;
-
-        uint random = result.get(originAmount - currentAmount);
-        uint reward = currentAmount == 1 ?
-            currentMoney :
-            (currentMoney.mul(10).div(currentAmount.mul(5))).mul(random).div(100);
-
-        currentAmount -= 1;
-        currentMoney -= reward;
-
-        (bool ok,) = payable(sender).call{value: reward}("");
-        require(ok, "Participate in failure");
-
-        if (currentAmount == 0) {
-            (bool a,) = factory.call(abi.encodeWithSignature("remove(uint256)", key));
-            (bool b,) = payable(owner).call{value: address(this).balance}("");
-            require(a && b, "Participate in failure!");
-        }
-
-        emit ParticipateEvent(sender, reward, random);
-    }
-    function fulfillRandomWords(uint256, uint256[] memory randomWords) internal override {
-        done = true;
-        for (uint32 i = 0; i < randomWords.length; i++) {
+    function fulfillRandomWords(uint256 requestID, uint256[] memory randomWords) internal override {
+        bytes32 ident = requestToIdent[requestID];
+        uint result;
+        for (uint i = 0; i < randomWords.length; i++) {
             uint random = randomWords[i];
             uint scope = random % 16;
             uint value = random % 2 == 0 ? 50 + scope : 50 - scope;
 
             result = result.set(i, value);
         }
+
+        Stocking storage gift = presents[ident];
+        gift.randomWord = result;
+
+        finished[ident] = true;
     }
 
-    function getRequestStatus() public view returns (uint) {
+    function getRequestStatus(bytes32 _ident) external view returns (uint) {
+        uint requestID = identToRequest[_ident];
         return uint(randomness.getRequestStatus(requestID));
+    }
+
+    function getCounter() external view returns (uint) {
+        return requestCounter;
+    }
+
+    function _getIdentifier(uint _key) internal view returns(bytes32) {
+        return keccak256(abi.encodePacked(_key, SALT, requestCounter));
+    }
+
+    function _getKeyLength(uint _key) internal pure returns (uint) {
+        return bytes(Strings.toString(_key)).length;
     }
 }
