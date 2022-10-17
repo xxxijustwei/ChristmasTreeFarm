e373ea928877f47d3dabb38f8a55e07bfab4c02a
justwei <admin@taylorswiftcn.com>
2022-10-18 00:19:42 +0800

    🚧 update

--- apps/src/routes/+layout.svelte
+++ apps/src/routes/+layout.svelte
@@ -1,5 +1,33 @@
 <script>
-  import "../app.postcss";
+    import "../app.postcss";
+    import Header from "./Header.svelte";
+    import "./styles.css";
 </script>
 
-<slot />
+<div class="app">
+    <Header />
+    <main>
+        <slot />
+    </main>
+
+    <footer></footer>
+</div>
+
+<style>
+    .app {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
--- apps/src/routes/+page.svelte
+++ apps/src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section>
+	<h1 class="title">
+		Welcome to ChristmasTreeFarm!
+	</h1>
+	<h1 class="text-3xl" style="color: #fecdd3;">coming soon</h1>
+</section>
+
+<style lang="less">
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.85;
+	}
+
+	.title {
+		line-height: 1;
+		font-size: 3rem;
+		font-weight: 300;
+		color: #10b981;
+		text-align: center;
+		width: 35rem;
+		margin: 0 auto;
+		font-variation-settings: 'wght' 100, 'wdth' 85;
+		animation: font-animation 4s infinite forwards cubic-bezier(0.48, 0.04, 0.52, 0.96);
+
+		@media screen and (max-width: 750px) {
+			font-size: 2rem;
+			width: 100%;
+		}
+	}
+
+</style>
